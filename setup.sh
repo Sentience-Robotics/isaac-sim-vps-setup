341b09b5c4120b91d9530778dfd66955e4b314cb
Arcod7 <antoine.esman@epitech.eu>
2025-04-26 19:58:07 +0200

    fix(omz): no more default interactive mode on omz

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ echo "dev:$DEV_PASSWORD" | chpasswd
 mkdir -p /home/dev/.ssh
 cp /home/user/.ssh/authorized_keys /home/dev/.ssh/authorized_keys
 
-chown -R dev:dev /home/dev/.ssh
+chown -R dev /home/dev/.ssh
 chmod 700 /home/dev/.ssh
 chmod 600 /home/dev/.ssh/authorized_keys
 
@@ -26,18 +26,15 @@ set -e
 
 # -- Install zsh --
 echo "$DEV_PASSWORD" | sudo -S apt-get update -y
-sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -yq
+sudo apt-get upgrade -yq
 sudo apt install zsh git vim -y
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="af-magic"/' ~/.zshrc
 sed -i '/^plugins=(/ s/)/ zsh-autosuggestions)/' ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 sudo chsh -s $(which zsh)
-
-echo "export 'DEBIAN_FRONTEND=noninteractive'" >> ~/.zshrc
-source ~/.zshrc
 
 # -- Install docker --
 
@@ -64,10 +61,11 @@ docker pull nvcr.io/nvidia/isaac-sim-comp-check:4.5.0
 docker run --name isaac-sim-comp-check --runtime=nvidia --gpus all -e "ACCEPT_EULA=Y" --rm --network=host -e "PRIVACY_CONSENT=Y" nvcr.io/nvidia/isaac-sim-comp-check:4.5.0
 
 # -- Install WireGuard --
+sufo ufw allow 51820
 sudo apt install -y wireguard
 
 # Generate WireGuard Server Keys
-umask 077 
+umask 077
 mkdir -p ~/.wireguard
 chmod 700 ~/.wireguard
 wg genkey | tee ~/.wireguard/server_private.key | wg pubkey > ~/.wireguard/server_public.key 
@@ -86,10 +84,10 @@ PostDown = iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
 # (Client peer configuration will be added here later)
 " | sudo tee /etc/wireguard/wg0.conf
 
-sudo sysctl -w net.ipv4.ip_forward=1 
+sudo sysctl -w net.ipv4.ip_forward=1
 
 # Start WireGuard service
-sudo systemctl start wg-quick@wg0 
+sudo systemctl start wg-quick@wg0
 sudo systemctl enable wg-quick@wg0
 
 # -- Install Isaac sim --
